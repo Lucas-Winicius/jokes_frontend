343426dd67b7a5760927c387610e264ab065abea
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-02 12:19:10 -0300

    LInk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import profileImage from "../../public/profile.svg";
 import Image from "next/image";
-import "./globals.css"
+import "./globals.css";
+import Link from "next/link";
 
 export const metadata = {
   title: "Jokeville",
@@ -12,19 +13,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-zinc-900 text-white">
         <nav className="flex justify-between p-3 px-6 items-center bg-zinc-800 shadow-lg">
-          <h1 className="text-4xl inline h-min carter-one">
-            Jokeville
-          </h1>
-          <div className="inline items-center cursor-pointer">
-            <Image
-              className="m-auto bg-white rounded-full"
-              src={profileImage}
-              alt="You Profile"
-              width={35}
-              height={35}
-            />
-            <span className="text-white">Profile</span>
-          </div>
+          <h1 className="text-4xl inline h-min carter-one">Jokeville</h1>
+          <Link href="/profile">
+            <div className="inline items-center cursor-pointer">
+              <Image
+                className="m-auto bg-white rounded-full"
+                src={profileImage}
+                alt="You Profile"
+                width={35}
+                height={35}
+              />
+              <span className="text-white">Profile</span>
+            </div>
+          </Link>
         </nav>
         {children}
       </body>
